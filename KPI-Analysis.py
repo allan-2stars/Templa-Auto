c6b505f5f7f0c52275c77e56da7dcbd0cd5f5b67
Allan Wang <wang.allan@outlook.com>
2019-06-11 12:19:58 +1000

    KPI analysis first 2 files export passed.

---
+++ KPI-Analysis.py
@@ -0,0 +1,153 @@
+from subprocess import Popen
+from pywinauto import Desktop
+from pywinauto import Application
+import pyautogui
+import pandas as pd
+from pandas import ExcelWriter
+from pandas import ExcelFile
+from pywinauto.application import Application
+import time
+import csv
+import os
+import sys
+import pywinauto
+from datetime import datetime
+
+## get Templa ready
+if (os.path.exists(r"E:\TCMS_LIVE\Client Suite")):
+    templa_file = r"E:\TCMS_LIVE\Client Suite\TemplaCMS32.exe"
+    app = Application(backend='uia').connect(path=templa_file)
+else:
+    print("Can't find Templa on your computer")
+
+templa = app.window(title='TemplaCMS  -  Contract Management System  --  TJS Services Group Pty Ltd LIVE')
+
+
+##### defined a function for save report into specific forlder repeatively ######
+
+def saveAsExcel(window, pathName, folderName, fileName):
+    ## export to excel and save
+    window.child_window(title="Excel", auto_id="[Group : report Tools] Tool : Report_Excel - Index : 2 ", control_type="Button").click_input()
+    saveAsWindow = window.child_window(title='Save As')
+    saveAsWindow.wait('exists', timeout=15)
+    print('save as window open')
+    addressBar = saveAsWindow.child_window(title_re="Address: *", control_type="ToolBar")
+    addressBar.click_input()
+    pyautogui.typewrite(pathName)
+    pyautogui.PAUSE = 2.5
+    pyautogui.press('enter')
+    ## add a new folder if not exists
+    folderNameNeeded = saveAsWindow.child_window(title=folderName, control_type="ListItem")
+    if not folderNameNeeded.exists():
+        print('folder NOT exists yet.')
+        saveAsWindow.child_window(title="New folder", auto_id="{E44616AD-6DF1-4B94-85A4-E465AE8A19DB}", control_type="Button").click_input()
+        pyautogui.PAUSE = 2.5
+        pyautogui.typewrite(folderName)
+        pyautogui.PAUSE = 2.5
+        pyautogui.press('enter')
+    ## get into the newly created folder
+    folderNameNeeded.click_input(button='left', double=True)
+    ## File name type
+    saveAsWindow.child_window(title="File name:", auto_id="FileNameControlHost", control_type="ComboBox").click_input()
+    pyautogui.typewrite(fileName)
+    ## Save button click
+    saveAsWindow.child_window(title="Save", auto_id="1", control_type="Button").click_input()
+    pyautogui.PAUSE = 5.5
+
+############### function end ########################
+
+########################
+#
+# Setup Excel Sheet
+#
+########################
+site_reallocate_sheet = 'KPI Analysis' 
+df = pd.read_excel('test.xlsx', sheet_name=site_reallocate_sheet)
+print("starting...")
+
+for i in df.index:
+    reportTitle = df['TITLE']
+    monthName = df['MONTH']
+    yearName = df['YEAR']
+    fileNameSiteTotals = df['FILE_NAME_SITE_TOTALS']
+    fileNameAllItems = df['FILE_NAME_ALL_ITEMS']
+    filePath = df['PATH']
+    status = df['STATUS']
+
+    if status[i] == "Done":
+        print(str(reportTitle[i]) + " is Done")
+        continue
+
+    if status[i] == "Stop":
+        print("Stop here")
+        break
+
+
+    ## start a report with title, need open one of the report analyser first
+    if i < 1:
+        previouseAnalysisWindow = app.window(title=reportTitle[i])
+    else: 
+        previouseAnalysisWindow = app.window(title=reportTitle[i-1])
+
+    analysisWindow = app.window(title=reportTitle[i])
+    ## open the report selection window
+    ## previouseAnalysisWindow['Select live report'].click_input()  ## too slow
+    liveReportButton = previouseAnalysisWindow.child_window(title="Select live report", auto_id="[Group : report Tools] Tool : Select - Index : 5 ", control_type="Button")
+    liveReportButton.wait('exists',10)
+    liveReportButton.click_input()
+    reportConfigWindow = previouseAnalysisWindow.child_window(title='QA Analysis Report Configurations')
+    reportConfigWindow.wait('exists', timeout=15)
+
+    ## type report title 
+    reportConfigWindow.window(title='Description', control_type='ComboBox').click_input()
+    pyautogui.typewrite(reportTitle[i])
+    pyautogui.moveRel(0, 25) 
+    pyautogui.click() # open the site by double click
+    reportConfigWindow.Select.click_input()
+    print('---------------', analysisWindow.title)
+    analysisWindow['Run report'].click_input()
+    print("Data loading ...")
+    
+    ## Header defined
+    siteHeader = analysisWindow.child_window(title="Site", auto_id="5", control_type="ComboBox")
+    siteHeader.wait('exists', timeout=120)
+    ## once the report loaded, start generating...
+    dragArea = analysisWindow.child_window(auto_id="GroupByBox", control_type="Group")
+    qaItemHeader = analysisWindow.child_window(title="QA Item",  control_type="ComboBox")
+    print('Data loaded, report generating ...')
+    ## Drag area title defined
+    siteDragArea = analysisWindow.child_window(title="Site", control_type="Button")
+    qaItemDragArea = analysisWindow.child_window(title="QA Item", control_type="Button")
+
+    ## drag "Site" Label up
+    siteHeader.click_input(button='left', double='true')
+    pyautogui.PAUSE = 2.5
+    pyautogui.moveRel(0, -20)
+    pyautogui.PAUSE = 2.5
+    pyautogui.dragRel(0,-70)
+
+
+    ## read below from excel sheet
+    folderName = monthName[i] + '-' + str(yearName[i])
+
+    saveAsExcel(analysisWindow, filePath[i], folderName , fileNameSiteTotals[i])
+
+    #analysisWindow.click_input()
+    ## drag "Site" Label down
+    pyautogui.PAUSE = 2.5
+    siteDragArea.click_input()
+    pyautogui.dragRel(0,60)
+
+    ## drag "QA Item" Label up
+    qaItemHeader.click_input(button='left', double='true')
+    pyautogui.PAUSE = 2.5
+    pyautogui.moveRel(0, -20)
+    pyautogui.PAUSE = 2.5
+    pyautogui.dragRel(0,-70)
+
+    saveAsExcel(analysisWindow, filePath[i], folderName , fileNameAllItems[i])
+
+    print(str(reportTitle[i]) + ": is Done now")
+    print("###############################")
+    print(" ")
+
--- testing.py
+++ testing.py
@@ -1,85 +1,3 @@
-# from subprocess import Popen
-# from pywinauto import Desktop
-# from pywinauto import Application
-# import pyautogui
-# import pandas as pd
-# from pandas import ExcelWriter
-# from pandas import ExcelFile
-# from pywinauto.application import Application
-# import time
-# import csv
-# import os
-# import sys
-# import pywinauto
-# from datetime import datetime
-
-
-# #print(templa)
-# #def generate_data_file(t_interval, interface_name, file_name):
-# # start Wireshark
-# if (os.path.exists(r"E:\TCMS_LIVE\Client Suite")):
-#     templa_file = r"E:\TCMS_LIVE\Client Suite\TemplaCMS32.exe"
-#     app = Application(backend='uia').connect(path=templa_file)
-# else:
-#     print("Can't find Templa on your computer")
-
-# templa = app.window(title='TemplaCMS  -  Contract Management System  --  TJS Services Group Pty Ltd LIVE')
-
-# ## start 
-
-# # templa.child_window(title='Product List', control_type='TabItem').click_input()
-# # mainProductsWindow = templa.child_window(title='Product List', control_type='Window')
-
-# print("starting...")
-
-# mainProductsWindow.window(title='Code', control_type='ComboBox').click_input()
-# print("printing description for template...")
-# pyautogui.typewrite("")
-
-
-
-
-# #######################################
-# #
-# # Copy old Products
-# #
-# #######################################
-# # click on other textbox first to deselect text
-# mainProductsWindow.window(title='Description', control_type='ComboBox').click_input()
-# # click on the Code Edit Box
-# mainProductsWindow.window(title='Code', control_type='ComboBox').click_input()
-
-
-# # for example copy product CODE URB120
-# existingCode = "URB120"
-# pyautogui.typewrite(existingCode)
-# mainProductsWindow.child_window(title=existingCode, control_type="DataItem").click_input()
-
-# templa.child_window(title="Copy", control_type="Button").click_input()
-
-# productDetailWindow = app.window(title_re='QA Item - *')
-# productDetailWindow.wait('exists', timeout=15)
-
-# #productDetailWindow.print_control_identifiers()
-# # Type code
-# productDetailWindow.child_window(auto_id="txtCode", control_type="Edit").click_input()
-# pyautogui.typewrite("URB2000")
-
-# # just tab will select all text, no need to clear manually
-# pyautogui.press('tab')
-# pyautogui.typewrite("test name")
-
- # print("general info filled")
- # Go to Price Group, change selling price
-# productDetailWindow.window(title='Price groups', control_type='TabItem').click_input()
-
-# priceGroupTextBox = productDetailWindow.child_window(title="Urbanest Room Cleaning", control_type="DataItem")
-# FixedPriceTextBox = priceGroupTextBox.child_window(title="Fixed price", control_type="Edit")
-# FixedPriceTextBox.click_input()
-# pyautogui.moveRel(20, 0)
-# pyautogui.click()
-
-
 from subprocess import Popen
 from pywinauto import Desktop
 from pywinauto import Application
@@ -101,6 +19,32 @@ def clearTextBySelectAll():
     pyautogui.dragRel(-500, 0, 1, button='left')
     pyautogui.press('del')
 
+def saveAsExcel(window, pathName, folderName, fileName):
+    ## export to excel and save
+    window.child_window(title="Excel", auto_id="[Group : report Tools] Tool : Report_Excel - Index : 2 ", control_type="Button").click_input()
+    saveAsWindow = window.child_window(title='Save As')
+    saveAsWindow.wait('exists', timeout=15)
+    print('save as window open')
+    addressBar = saveAsWindow.child_window(title_re="Address: *", control_type="ToolBar")
+    addressBar.click_input()
+    pyautogui.typewrite(pathName)
+    pyautogui.press('enter')
+    ## add a new folder if not exists
+    folderNameNeeded = saveAsWindow.child_window(title=folderName, control_type="ListItem")
+    if not folderNameNeeded.exists():
+        print('folder NOT exists yet.')
+        saveAsWindow.child_window(title="New folder", auto_id="{E44616AD-6DF1-4B94-85A4-E465AE8A19DB}", control_type="Button").click_input()
+        pyautogui.typewrite(folderName)
+        pyautogui.press('enter')
+    ## get into the newly created folder
+    folderNameNeeded.click_input(button='left', double=True)
+    ## File name type
+    saveAsWindow.child_window(title="File name:", auto_id="FileNameControlHost", control_type="ComboBox").click_input()
+    pyautogui.typewrite(fileName)
+    ## Save button click
+    saveAsWindow.child_window(title="Save", auto_id="1", control_type="Button").click_input()
+    
+
 #print(templa)
 #def generate_data_file(t_interval, interface_name, file_name):
 # start Wireshark
@@ -112,17 +56,65 @@ else:
 
 templa = app.window(title='TemplaCMS  -  Contract Management System  --  TJS Services Group Pty Ltd LIVE')
 
-## start 
-mainSitesTab = templa.child_window(title='Workflow Manager', control_type='TabItem')
-mainSitesTab.click_input()
-WorkflowManagerWindow = templa.child_window(title='Workflow Manager', control_type='Window')
-ResponsibleBox = WorkflowManagerWindow.child_window(title="Responsible", auto_id="1", control_type="ComboBox")
-detailBox = WorkflowManagerWindow.child_window(title="Details", auto_id="2", control_type="ComboBox")
-detailBox.click_input()
-pyautogui.typewrite("test")
-csmAlreadyAssigned = ResponsibleBox.child_window(title="Allan Test", control_type="DataItem")
-print(csmAlreadyAssigned.exists())
+## start a report with title 
+analysisWindow = app.window(title='Affinity Monthly')
 
 
-#mainSitesWindow.print_control_identifiers()
+analysisWindow['Select live report'].click_input()
+
+## open site selection window
+reportConfigWindow = analysisWindow.child_window(title='QA Analysis Report Configurations')
+reportConfigWindow.wait('exists', timeout=15)
+
+## type report title 
+reportConfigWindow.window(title='Description', control_type='ComboBox').click_input()
+pyautogui.typewrite('Affinity Monthly')
+pyautogui.moveRel(0, 25) 
+pyautogui.click() # open the site by double click
+reportConfigWindow.Select.click_input()
+
+ analysisWindow['Run report'].click_input()
+print("loading data...")
+## Header defined
+dragArea = analysisWindow.child_window(auto_id="GroupByBox", control_type="Group")
+siteHeader = analysisWindow.child_window(title="Site", auto_id="5", control_type="ComboBox")
+qaItemHeader = analysisWindow.child_window(title="QA Item",  control_type="ComboBox")
+
+## Drag area title defined
+siteDragArea = analysisWindow.child_window(title="Site", control_type="Button")
+qaItemDragArea = analysisWindow.child_window(title="QA Item", control_type="Button")
+
+## drag "Site" Label up
+siteHeader.click_input(button='left', double='true')
+pyautogui.PAUSE = 2.5
+pyautogui.moveRel(0, -20)
+pyautogui.PAUSE = 2.5
+pyautogui.dragRel(0,-70)
+
+
+## read below from excel sheet
+monthName = 'May-2019'
+pathName = 'C:\Profiles\\awang\My Documents\Report Monthly KPI\Affinity'
+
+saveAsExcel(analysisWindow, pathName, monthName , 'site totals')
+
+#analysisWindow.click_input()
+## drag "Site" Label down
+pyautogui.PAUSE = 2.5
+siteDragArea.click_input()
+pyautogui.dragRel(0,60)
+
+## drag "QA Item" Label up
+qaItemHeader.click_input(button='left', double='true')
+pyautogui.PAUSE = 2.5
+pyautogui.moveRel(0, -20)
+pyautogui.PAUSE = 2.5
+pyautogui.dragRel(0,-70)
+
+saveAsExcel(analysisWindow, pathName, monthName , 'all items')
+
+# print(reportConfigWindow.exists())
+
+
+#analysisWindow.print_control_identifiers()
 
