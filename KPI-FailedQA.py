b96a67d1c350558afe382594cb64a6e06d6b5150
Allan Wang <wang.allan@outlook.com>
2019-06-13 12:14:13 +1000

    qa item filter added basic

--- KPI-FailedQA.py
+++ KPI-FailedQA.py
@@ -31,7 +31,37 @@ templa = app.window(title='TemplaCMS  -  Contract Management System  --  TJS Ser
 filterWindow = templa.window(title_re='QA Completed Item Filter Detail - *')
 filterWindow.wait('exists', timeout=15)
 
-## change the filters criteria
+## default filter
+filterWindow.child_window(title="Default criteria").click_input()
+## change the QA filters criteria
+QAFilterCriteria = filterWindow.child_window(title='QA filtering criteria', control_type='TabItem')
+QAFilterCriteria.click_input()
+
+###############################################
+############                    ###############
+############  Basic Filtering   ###############
+
+## filter on date range of audited date
+filterWindow.child_window(auto_id="datAuditDateFrom", control_type="Edit").click_input()
+pyautogui.typewrite('01062019')
+filterWindow.child_window(auto_id="datAuditDateTo", control_type="Edit").click_input()
+pyautogui.typewrite('30062019')
+
+## ## click on Failed Items button to YES
+pyautogui.press('tab')
+pyautogui.press('tab')
+pyautogui.press('tab')
+pyautogui.press('right')
+pyautogui.press('space')
+
+## if the site is PMC and DAWR then use Ignore.
+# just Default the filter will do the trick
+
+###########   End of Basic Filtering    #########
+#################################################
+
+
+## change the site filters criteria
 siteFilterCriteria = filterWindow.child_window(title='Site filtering criteria', control_type='TabItem')
 siteFilterCriteria.click_input()
 filterWindow.child_window(title="Contracts", auto_id="5", control_type="DataItem").click_input()
@@ -40,5 +70,5 @@ pyautogui.dragRel(-300,0)
 pyautogui.typewrite('Affinity')
 pyautogui.press('tab')
 
-## Save the filter
+# ## Save the filter
 filterWindow.Save.click_input()
