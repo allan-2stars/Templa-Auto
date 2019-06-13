b6b69ab719dfea11a6d8c5f72c313d87ea98370e
Allan Wang <wang.allan@outlook.com>
2019-06-13 12:57:26 +1000

    add in excel sheet reading

--- KPI-FailedQA.py
+++ KPI-FailedQA.py
@@ -23,52 +23,117 @@ else:
 templa = app.window(title='TemplaCMS  -  Contract Management System  --  TJS Services Group Pty Ltd LIVE')
 
 
-## start 
-# completedQAWindow = templa.child_window(title='Completed QA Items', control_type='TabItem')
-# completedQAWindow.click_input()
+########################
+#
+# Setup Excel Sheet
+#
+########################
+Work_Sheet = 'KPI QA Completed Items' 
+df = pd.read_excel('test.xlsx', sheet_name=Work_Sheet)
+print("starting...")
 
-# templa['Change filter'].click_input()
-filterWindow = templa.window(title_re='QA Completed Item Filter Detail - *')
-filterWindow.wait('exists', timeout=15)
+for i in df.index:
+    constracts = df['CONTRACTS']
+    siteName = df['SITE NAME']
+    client = df['CLIENT']
+    template = df['TEMPLATE']
+    filePath = df['PATH']
+    fileName = df['FILE_NAME_FAILED_QA_ITEMS']
+    status = df['STATUS']
 
-## default filter
-filterWindow.child_window(title="Default criteria").click_input()
-## change the QA filters criteria
-QAFilterCriteria = filterWindow.child_window(title='QA filtering criteria', control_type='TabItem')
-QAFilterCriteria.click_input()
+    useContract = df['USE CONTRACTS']
+    useSite = df['USE SITE']
+    useClient = df['USE CLIENT']
+    useTemplate = df['USE TEMPLATE']
 
-###############################################
-############                    ###############
-############  Basic Filtering   ###############
+    #print("Site Name:" + siteName[i])
+    #print("CSM: " + csm[i])
+    #print("iPad: " + ipad[i])
+    if status[i] == "Done":
+        print(str(siteName[i]) + " is Done")
+        continue
 
-## filter on date range of audited date
-filterWindow.child_window(auto_id="datAuditDateFrom", control_type="Edit").click_input()
-pyautogui.typewrite('01062019')
-filterWindow.child_window(auto_id="datAuditDateTo", control_type="Edit").click_input()
-pyautogui.typewrite('30062019')
+    if status[i] == "Stop":
+        print("Stop here")
+        break
 
-## ## click on Failed Items button to YES
-pyautogui.press('tab')
-pyautogui.press('tab')
-pyautogui.press('tab')
-pyautogui.press('right')
-pyautogui.press('space')
+    ## start 
+    completedQAWindow = templa.child_window(title='Completed QA Items', control_type='TabItem')
+    completedQAWindow.click_input()
 
-## if the site is PMC and DAWR then use Ignore.
-# just Default the filter will do the trick
+    # templa['Change filter'].click_input()
+    filterWindow = templa.window(title_re='QA Completed Item Filter Detail - *')
+    filterWindow.wait('exists', timeout=15)
 
-###########   End of Basic Filtering    #########
-#################################################
+    ## default filter
+    filterWindow.child_window(title="Default criteria").click_input()
+    ## change the QA filters criteria
+    QAFilterCriteria = filterWindow.child_window(title='QA filtering criteria', control_type='TabItem')
+    QAFilterCriteria.click_input()
+
+    if useTemplate[i] == 'Yes':
+        ## Use Template Filter
+        filterWindow.child_window(title="QA template", control_type="DataItem").click_input()
+        pyautogui.moveRel(100,0)
+        pyautogui.dragRel(-300,0)
+        pyautogui.typewrite(template[i])
+        pyautogui.press('tab')
+
+    # ###############################################
+    # ############                    ###############
+    # ############  Basic Filtering   ###############
+
+    # ## filter on date range of audited date
+    # filterWindow.child_window(auto_id="datAuditDateFrom", control_type="Edit").click_input()
+    # pyautogui.typewrite('01062019')
+    # filterWindow.child_window(auto_id="datAuditDateTo", control_type="Edit").click_input()
+    # pyautogui.typewrite('30062019')
+
+    # ## ## click on Failed Items button to YES
+    # pyautogui.press('tab')
+    # pyautogui.press('tab')
+    # pyautogui.press('tab')
+    # pyautogui.press('right')
+    # pyautogui.press('space')
+
+    # ## if the site is PMC and DAWR then use Ignore.
+    # # just Default the filter will do the trick
+
+    # ###########   End of Basic Filtering    #########
+    # #################################################
 
 
-## change the site filters criteria
-siteFilterCriteria = filterWindow.child_window(title='Site filtering criteria', control_type='TabItem')
-siteFilterCriteria.click_input()
-filterWindow.child_window(title="Contracts", auto_id="5", control_type="DataItem").click_input()
-pyautogui.moveRel(100,0)
-pyautogui.dragRel(-300,0)
-pyautogui.typewrite('Affinity')
-pyautogui.press('tab')
+    ## change the site filters criteria
+    siteFilterCriteria = filterWindow.child_window(title='Site filtering criteria', control_type='TabItem')
+    siteFilterCriteria.click_input()
 
-# ## Save the filter
-filterWindow.Save.click_input()
+
+    if useContract[i] == 'Yes':
+        ## Use Contracts filter
+        filterWindow.child_window(title="Contracts", auto_id="5", control_type="DataItem").click_input()
+        pyautogui.moveRel(100,0)
+        pyautogui.dragRel(-300,0)
+        pyautogui.typewrite(constracts[i])
+        pyautogui.press('tab')
+
+    if useSite[i] == 'Yes':
+        ## Use Site Filter
+        filterWindow.child_window(title="Site", control_type="DataItem").click_input()
+        pyautogui.moveRel(100,0)
+        pyautogui.dragRel(-300,0)
+        pyautogui.typewrite(siteName[i])
+        pyautogui.press('tab')
+
+    if useClient[i] == 'Yes':
+        ## Use Client Filter
+        filterWindow.child_window(title="Client", control_type="DataItem").click_input()
+        pyautogui.moveRel(100,0)
+        pyautogui.dragRel(-300,0)
+        pyautogui.typewrite(client[i])
+        pyautogui.press('tab')
+
+
+
+
+    # ## Save the filter
+    filterWindow.Save.click_input()
