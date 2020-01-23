8eddf85cc228401d9d95a1a30858c7cbe004fd19
Allan Wang <wang.allan@outlook.com>
2020-01-23 17:00:47 +1100

    product name updating feature

---
+++ ProductChangeName.py
@@ -0,0 +1,100 @@
+from pywinauto import Desktop
+from pywinauto import Application
+import pyautogui
+import pandas as pd
+from pandas import ExcelWriter
+from pandas import ExcelFile
+from pywinauto.application import Application
+import time
+import csv
+import os
+import sys
+import pywinauto
+from datetime import datetime
+
+
+if (os.path.exists(r"E:\TCMS_LIVE\Client Suite")):
+    templa_file = r"E:\TCMS_LIVE\Client Suite\TemplaCMS32.exe"
+    app = Application(backend='uia').connect(path=templa_file)
+else:
+    print("Can't find Templa on your computer")
+
+templa = app.window(title='TemplaCMS  -  Contract Management System  --  TJS Services Group Pty Ltd LIVE')
+
+## start 
+templa.child_window(title='Product List', control_type='TabItem').click_input()
+mainProductsWindow = templa.child_window(title='Product List', control_type='Window')
+
+########################
+#
+# Setup Excel Sheet
+#
+########################
+sheetLoader = 'Change Product Name' 
+df = pd.read_excel('test.xlsx', sheet_name=sheetLoader)
+print("starting...")
+
+for i in df.index:
+    product_code = df['PRODUCT-CODE']
+    product_name = df['ITEMS']
+    status = df['STATUS']
+
+
+    if status[i] == "Done" or status[i] == "Skip":
+        print(str(product_code[i]) + " is Done")
+        continue
+
+    if status[i] == "Stop":
+        print("Stop here")
+        break
+
+
+    # trim the space on leading and tailing, in case excel sheet code un-stripped.
+    product_code_text = str(product_code[i]).strip()
+
+    # # If the product exists
+    # # click on the "Code" Edit Box
+    mainProductsWindow.window(title='Code', control_type='ComboBox').click_input()
+    pyautogui.typewrite(product_code_text)
+
+
+    # check if the Part Number is match the Product code, move Part no. on the first column
+    productItem = mainProductsWindow.child_window(title=product_code_text, control_type="DataItem")
+    # get category and ready for check exisitance
+
+    productDetailWindow = app.window(title_re='Products - *')
+    # if the product exists already, then directly open it.
+    if productItem.exists():
+        print('Product ' + product_code_text + ' Exists, open it now ...')
+        pyautogui.moveRel(-25, 25) # move the cursor and selec the product
+        pyautogui.doubleClick() # open the existing product by double click
+    
+        #######################################
+        #
+        # Update Products
+        #
+        #######################################        
+        productDetailWindow.wait('exists', timeout=15)
+        pyautogui.press('tab')
+        ## productDetailWindow.child_window(auto_id="txtDescription", control_type="Edit").click_input()
+        pyautogui.typewrite(product_name[i])
+
+        print("Product name/description changed ...")
+
+        productDetailWindow.Save.click_input()
+        pyautogui.PAUSE = 2.5
+        print (product_code_text + " product name updated.")
+    else:
+        print('-----------------------------------------------------------')
+        print('-- Warning! Product ' + product_code_text + ' not found, please check !!! --')
+        print('-----------------------------------------------------------')
+        continue
+
+    print ("###################################")
+    print (" ")
+
+
+
+    
+
+
