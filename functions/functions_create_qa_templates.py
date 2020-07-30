09d6850cedcb88101a1e73d6f7aaca59eb1b437d
Allan Wang <wang.allan@outlook.com>
2020-07-30 14:28:39 +1000

    try except else the if the last line of entries, collapse into section title, for saving space of screen

--- functions/functions_create_qa_templates.py
+++ functions/functions_create_qa_templates.py
@@ -23,14 +23,7 @@ def Create_QA_Templates():
         print("Starting...")
         mainQATemplateTab = templa.child_window(title='QA Templates', control_type='TabItem')
         mainQATemplateTab.click_input()
-        ## mainQATemplateWindow = templa.child_window(title='QA Templates', control_type='Window')
 
-        print("click New button ...")
-        new_button = templa.child_window(title="New", auto_id="[Group : row Tools] Tool : list_New - Index : 1 ", control_type="Button")
-        new_button.click_input()
-        QATemplateWindow = app.window(title_re='QA Template*')
-        QATemplateWindow.wait('exists', timeout=25)
-        #QATemplateWindow.print_control_identifiers()
         ########################
         #
         # Setup Excel Sheet
@@ -52,7 +45,7 @@ def Create_QA_Templates():
                 print(description[i] + " is Done")
                 continue
 
-            if status[i] == "Same Section":
+            if status[i] == "Same Template":
                 ## no need print out infomation
                 ## undless in debug mode
                 # print(description[i] + " is under Same Section")
@@ -66,6 +59,14 @@ def Create_QA_Templates():
                 print("Stop here")
                 break
 
+
+            print("click New button ...")
+            new_button = templa.child_window(title="New", auto_id="[Group : row Tools] Tool : list_New - Index : 1 ", control_type="Button")
+            new_button.click_input()
+            QATemplateWindow = app.window(title_re='QA Template*')
+            QATemplateWindow.wait('exists', timeout=25)
+
+            print('New Template Creating ...')
             description_textbox = QATemplateWindow.child_window(auto_id="txtDescription", control_type="Edit")
             description_textbox.click_input()
             pyautogui.typewrite(description[i])
@@ -101,6 +102,7 @@ def Create_QA_Templates():
 
             ## Start QA items section
             ## add Overall criteria
+            print('Adding QA Items to every Section ...')
             QATemplateWindow.child_window(title="Overall criteria", auto_id="[Node] 0", control_type="DataItem").click_input()
             QATemplateWindow.child_window(title="Add QA criteria", auto_id="[Group : structure Tools] Tool : Item_AddCriteria - Index : 7 ", control_type="Button").click_input()
             QACriteriaWindow = app.window(title_re='QA Criteria')
@@ -118,13 +120,16 @@ def Create_QA_Templates():
             ## Loop to every section here
             while True:
                 i = i + 1
-                QATemplateWindow.child_window(title=str(sections[i]), control_type="DataItem").click_input()
+                section_title = QATemplateWindow.child_window(title=str(sections[i]), control_type="DataItem")
+                section_title.click_input()
                 add_qa_item_button.click_input()
 
                 QAItemsWindow = app.window(title_re='QA Items')
                 QAItemsWindow.wait('exists', timeout=25)
                 QAItemsWindow.child_window(title="Group", control_type="ComboBox").click_input()
                 pyautogui.typewrite(str(qa_item_groups[i]))
+                
+                print('Adding Items under section: ' + str(sections[i]))
                 ## Loop to add qa items for each section here
                 while True:
                     QAItemsWindow.child_window(title="Description", control_type="ComboBox").click_input()
@@ -132,13 +137,33 @@ def Create_QA_Templates():
                     pyautogui.moveRel(-25, 25) 
                     pyautogui.click() # open the site by double click
                     QAItemsWindow.Select.click_input()
+                    print('Added item: ' + qa_items[i])
                     ## check if next items are in the same seciont,
                     ## if not, jump out of the loop
-                    if sections[i] != sections[i+1]:
+                    try:
+                        next_section = sections[i+1]
+                    except:
+                        print('last line, no more section !')
+                        print('---------------------------')
                         QAItemsWindow.Close.click_input()
                         break
+                    else:
+                        if sections[i] != sections[i+1]:
+                            QAItemsWindow.Close.click_input()
+                            break
                     i = i + 1
-                
-                if description[i] != description[i+1]:
+
+                QATemplateWindow.wait('exists', timeout=10)
+                section_title.click_input(button='left', double=True) ## to collapse items save screen space
+
+                try:
+                    next_template = description[i+1]
+                except:
+                    print('no more template need to be created, exit !')
                     QATemplateWindow.Save.click_input()
                     break
+                else:
+                    if description[i] != description[i+1]:
+                        QATemplateWindow.Save.click_input()
+                        break
+                
